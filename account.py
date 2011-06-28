c42d9295272c82e5d75a750529ddb071f8e8546e
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2011-06-28 13:48:07 -0500

    Finished implementing the project wrapper

--- account.py
+++ account.py
@@ -1,5 +1,6 @@
 import urllib2
 import json
+import project
 
 class Account(object):
 	"""Account wrapper for Sifter"""
@@ -9,10 +10,15 @@ class Account(object):
 	
 	def projects(self):
 		"""Gets all the projects from sifter"""
+		projects = list()
 		url = self.host + '/api/projects'
 		req = urllib2.Request(url)
 		req.add_header('X-Sifter-Token', self.token)
 		json_raw = json.loads(urllib2.urlopen(req).read())
-		projects = json_raw['projects']
+		raw_projects = json_raw['projects']
+		for raw_project in raw_projects:
+			p = project.Project(raw_project)
+			projects.append(p)
+		
 		return projects
 	
--- project.py
+++ project.py
@@ -1,4 +1,13 @@
 class Project(object):
 	"""Representation of a project in Sifter"""
 	def __init__(self, project):
+		self.issues_url = project['issues_url']
+		self.archived = project['archived']
+		self.url = project['url']
+		self.milestones_url = project['milestones_url']
+		self.api_people_url = project['api_people_url']
+		self.api_issues_url = project['api_issues_url']
+		self.api_milestones_url = project['api_milestones_url']
+		self.api_categories_url = project['api_categories_url']
+		self.name = project['name']
 	

026cd20b5084931b036a6e949f4f1f48cd907cd7
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2011-06-28 11:25:46 -0500

    Added a frame of a method to get all the projects.  Importing urllib2 as well.

--- account.py
+++ account.py
@@ -1,7 +1,12 @@
+import urllib2
+
 class Account(object):
 	"""Account wrapper for Sifter"""
 	def __init__(self, host, token):
 		self.host = host
 		self.token = token
 	
+	def projects(self):
+		"""Gets all the projects from sifter"""
+		pass
 	

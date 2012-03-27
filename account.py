de07e79e7e59290c391053e3d5e18e4b94307e24
Mark Mikofski <bwanamarko@yahoo.com>
2012-03-27 01:57:51 -0700

    use requests, add comments

--- account.py
+++ account.py
@@ -1,25 +1,28 @@
-import urllib2
+import requests
 import json
 import project
 
 class Account(object):
-	"""Account wrapper for Sifter"""
-	def __init__(self, host, token):
-		self.host = host
-		self.token = token
-	
-	def projects(self):
-		"""Gets all the projects from sifter"""
-		projects = []
-		url = self.host + '/api/projects'
-		req = urllib2.Request(url)
-		req.add_header('X-Sifter-Token', self.token)
-		req.add_header('Accept', 'application/json')
-		json_raw = json.loads(urllib2.urlopen(req).read())
-		raw_projects = json_raw['projects']
-		for raw_project in raw_projects:
-			p = project.Project(raw_project, self.token)
-			projects.append(p)
-		
-		return projects
-	
+    """Account wrapper for Sifter"""
+    def __init__(self, host, token):
+        self.host = host
+        self.token = token
+        self.url = 'https://' + self.host + '.sifterapp.com' + '/api/projects'
+
+    def request(self, url):
+        """Requets JSON object from Sifter url"""
+        req = requests.get(url, headers={'X-Sifter-Token':self.token,
+                                         'Accept':'application/json'})
+        
+        return json.loads(req.content)
+            
+    def projects(self):
+        """Gets all the projects from sifter"""
+        projects = []
+        json_raw = self.request(self.url)
+        raw_projects = json_raw['projects']
+        for raw_project in raw_projects:
+            proj = project.Project(raw_project, self)
+            projects.append(proj)
+        
+        return projects
--- issue.py
+++ issue.py
@@ -1,15 +1,32 @@
+import comment
+
 class Issue(object):
-	"""Representation of an Issue in Sifter"""
-	def __init__(self, issue):
-		self.number = issue['number']
-		self.subject = issue['subject']
-		self.description = issue['description']
-		self.priority = issue['priority']
-		self.status = issue['status']
-		self.assignee_name = issue['assignee_name']
-		self.category_name = issue['category_name']
-		self.milestone_name = issue['milestone_name']
-		self.opener_name = issue['opener_name']
-		self.url = issue['url']
-		self.api_url = issue['api_url']
-	
+    """Representation of an Issue in Sifter"""
+    def __init__(self, issue, account):
+        self._account = account
+        self.number = issue['number']
+        self.subject = issue['subject']
+        self.description = issue['description']
+        self.priority = issue['priority']
+        self.status = issue['status']
+        self.assignee_name = issue['assignee_name']
+        self.category_name = issue['category_name']
+        self.milestone_name = issue['milestone_name']
+        self.opener_name = issue['opener_name']
+        self.url = issue['url']
+        self.api_url = issue['api_url']
+        self.comment_count = issue['comment_count']
+        self.created_at = issue['created_at']
+        self.updated_at = issue['updated_at']
+    
+    def comments(self):
+        """Gets Comments for a given Issue"""
+        comments = []
+        json_raw = self._account.request(self.api_url)
+        
+        raw_comments = json_raw['comments']
+        for raw_comment in raw_comments:
+            c = comment.Comment(raw_comment)
+            comments.append(c)
+        
+        return comments
